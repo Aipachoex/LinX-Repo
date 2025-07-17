8394e709668f9521e433ad8a29044e22c571e25e
AidanPacheco <aidanrpacheco@gmail.com>
2025-07-17 14:46:15 +0100

    merging of code and function additions

--- letsexplore.sh
+++ letsexplore.sh
@@ -4,7 +4,7 @@
 
 north_door() {
 	clear
-	echo -e "\nYou walk through the North door and it quickly slams behind you, it is now locked."
+	echo "You walk through the North door and it quickly slams behind you, it is now locked."
 	sleep 2
 	echo -e "\nThe room looks similar to the previous room with three more doors, however there is something in the middle of the room."
 	sleep 2
@@ -26,8 +26,31 @@ north_door() {
 			echo -e "\nShall we try that again?"
 			sleep 2
 		fi
-	done
-	
+	done	
+	echo -e "\nYou look back at the 3 new doors before you, where would you like to go now?"
+    sleep 2
+    echo -e "\nNorth, East, or West?"
+    sleep 2
+    while true; do
+        read -rp "\nPlease type in a direction: " dir2
+        case "${dir2,,}" in
+            "north")
+            #north_bronze function here
+            break
+            ;;
+            "east")
+            #east_bronze function here
+            break
+            ;;
+            "west")
+            #west_bronze function here
+            break
+            ;;
+            *)
+            echo -e "\nLet's try that again shall we?"
+            ;;
+    	esac
+    done
 }
 
 
@@ -56,8 +79,134 @@ south_door() {
 			sleep 2
 		fi
 	done
+	echo -e "\nYou look back at the 3 new doors before you, where would you like to go now?"
+    	sleep 2
+    	echo -e "\nNorth, East, or West?"
+    	sleep 2
+    	while true; do
+        read -rp "\nPlease type in a direction: " dir3
+        case "${dir3,,}" in
+            "north")
+            #north_silver function here
+            break
+            ;;
+            "east")
+            #east_silver function here
+            break
+            ;;
+            "west")
+            #west_silver function here
+            break
+            ;;
+            *)
+            echo -e "\nLet's try that again shall we?"
+            ;;
+    	esac
+    done
+
 }
 
+east_door() {
+	clear
+	echo "You walk through the East door and it quickly slams behind you, it is now locked."
+	sleep 2
+	echo -e "\nThe room looks similar to the previous room with three more doors, however there is something in the middle of the room."
+	sleep 2
+	echo -e "\nOn closer inspection the item appears to be a small gold coin."
+	sleep 2
+	while true; do
+		echo -e "\nWill you pick it up or leave it?"
+		read -rp "Type yes or no: " goldcoin
+		if [[ "${goldcoin,,}" == "yes" ]]; then
+			sleep 2
+			echo -e "\nYou pick up the gold coin and put it in your pocket."
+			break
+		elif [[ "${goldcoin,,}" == "no" ]]; then
+			sleep 2
+			echo -e "\nYou leave the gold coin where it is."
+			break
+		else
+			sleep 2
+			echo -e "\nShall we try that again?"
+			sleep 2
+		fi
+	done	
+	echo -e "\nYou look back at the 3 new doors before you, where would you like to go now?"
+    sleep 2
+    echo -e "\nNorth, East, or West?"
+    sleep 2
+    while true; do
+        read -rp "\nPlease type in a direction: " dir4
+        case "${dir4,,}" in
+            "north")
+            #north_gold function here
+            break
+            ;;
+            "east")
+            #east_gold function here
+            break
+            ;;
+            "west")
+            #west_gold function here
+            break
+            ;;
+            *)
+            echo -e "\nLet's try that again shall we?"
+            ;;
+    	esac
+    done
+}
+
+west_door() {
+	clear
+	echo "You walk through the West door and it quickly slams behind you, it is now locked."
+	sleep 2
+	echo -e "\nThe room looks similar to the previous room with three more doors, however there is something in the middle of the room."
+	sleep 2
+	echo -e "\nOn closer inspection the item appears to be a small platinum coin."
+	sleep 2
+	while true; do
+		echo -e "\nWill you pick it up or leave it?"
+		read -rp "Type yes or no: " platcoin
+		if [[ "${platcoin,,}" == "yes" ]]; then
+			sleep 2
+			echo -e "\nYou pick up the platinum coin and put it in your pocket."
+			break
+		elif [[ "${platcoin,,}" == "no" ]]; then
+			sleep 2
+			echo -e "\nYou leave the platinum coin where it is."
+			break
+		else
+			sleep 2
+			echo -e "\nShall we try that again?"
+			sleep 2
+		fi
+	done	
+	echo -e "\nYou look back at the 3 new doors before you, where would you like to go now?"
+    sleep 2
+    echo -e "\nNorth, East, or West?"
+    sleep 2
+    while true; do
+        read -rp "\nPlease type in a direction: " dir5
+        case "${dir5,,}" in
+            "north")
+            #north_platinum function here
+            break
+            ;;
+            "east")
+            #east_platinum function here
+            break
+            ;;
+            "west")
+            #west_platinum function here
+            break
+            ;;
+            *)
+            echo -e "\nLet's try that again shall we?"
+            ;;
+    	esac
+    done
+}
 
 intro_scene() {
 	echo "Let us begin your journey."
@@ -80,11 +229,11 @@ intro_scene() {
 			break
 			;;
 			"east")
-			#East_door function here
+			east_door
 			break
 			;;
 			"west")
-			#West_door function here
+			west_door
 			break
 			;;
 			*)
